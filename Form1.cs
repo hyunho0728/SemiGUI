031bcae0be6e9745b1bb2fa79aca84a87bc16251
hyunho0728 <yu16891@gmail.com>
2025-11-24 23:02:47 +0900

    feat : db 연결 실패시 종료 기능 추가

--- Form1.cs
+++ Form1.cs
@@ -58,7 +58,7 @@ namespace SemiGUI
         {
             using (MySqlConnection conn = new MySqlConnection(connectDB))
             {
-                bool inputDummy = true; // 더미 데이터 입력 여부
+                bool inputDummy = false; // 더미 데이터 입력 여부
 
                 try
                 {
@@ -88,10 +88,9 @@ namespace SemiGUI
                     MySqlCommand cmd = new MySqlCommand();
                     cmd.Connection = conn;
 
-                    MySqlCommand vcmd = new MySqlCommand("select version();", conn);
-                    string version = vcmd.ExecuteScalar().ToString();
-
-                    MessageBox.Show("현재 MySQL 버전: " + version);
+                    //MySqlCommand vcmd = new MySqlCommand("select version();", conn);
+                    //string version = vcmd.ExecuteScalar().ToString();
+                    //MessageBox.Show("현재 MySQL 버전: " + version);
 
                     foreach (string q in query)
                     {
@@ -102,6 +101,7 @@ namespace SemiGUI
                 catch (Exception ex)
                 {
                     MessageBox.Show("DB 연결 실패: " + ex.Message);
+                    Environment.Exit(1);
                 }
             }
         }
